636c463cab93c9c53440ba52af71ee6be00114a7
blake <blake@meishakeji.com>
2020-03-15 16:29:39 +0800

    feat: 手写防抖和节流

--- 工具函数/节流.js
+++ 工具函数/节流.js
@@ -1,7 +1,15 @@
-function throttle(fn, time) {
-  let timeId = null
+// 特点：稀释操作，一定时间内只执行一次，需要有一个标志位看能否执行
+
+function throttle(fn, delay) {
+  let canRun = true
   return function() {
-    clearTimeout(timeId)
-    timeId = setTimeout(fn, time)
+    if (!canRun) return
+    canRun = false
+    fn.apply(this, arguments) // 这个也可以放在下面的 setTimeout 中，具体看你是要在一开始执行还是结束之后执行
+    setTimeout(() => {
+      canRun = true
+    }, delay)
   }
 }
+
+// 当然 setTimeout 可能不准，所以可以用 new Date()
---
+++ 工具函数/防抖.js
@@ -0,0 +1,43 @@
+// 防止多次执行，最终在一定的时间间隔内只执行一次，比如拖拽窗口
+// 特点：无限延迟，就像照相，什么时候手不抖了就执行
+
+function debounce(fn, wait=300, immediate=false) {
+  let timer, result
+  let d =  function(...args) {
+    timer && clearTimeout(timer)
+    if (immediate) { // 是否要立即执行
+      let callNow = !timer
+      timer = setTimeout(() => {
+        timer = null
+      }, wait)
+      if (callNow) result = fn.apply(this, args) // 即便里面报错也会延缓执行
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, args) // 这是为了防止改变 this 指向
+      }, wait)
+    }
+    return result // 这个只有在立即执行才有意义
+  }
+  d.cancel = function() {
+    clearTimeout(timer)
+  }
+  return d
+}
+
+function betterDebounce(fn, delay=300, maxTime) { // 如果一直抖动就会一直不执行感觉像是卡顿，所以可以设置一个 maxTime
+  let last = +new Date()
+  let timer = null
+  return function() {
+    let now = +new Date()
+    timer && clearTimeout(timer)
+    if (now - last < maxTime) {
+      timer = setTimeout(() => {
+        fn.apply(this, arguments)
+        last = now
+      }, delay);
+    } else {
+      fn.apply(this, arguments)
+      last = now
+    }
+  }
+}

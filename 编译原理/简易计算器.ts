9c02f661af082fa37828b9262b1e2cf5c9eaae43
blake <blake@meishakeji.com>
2020-11-22 15:02:26 +0800

    feat: 计算器

--- 编译原理/简易计算器.ts
+++ 编译原理/简易计算器.ts
@@ -11,3 +11,70 @@ console.log(token)
 if (token && token.getType() === TokenType.Let) {
 
 }
+
+
+function Expression(tokens){
+  if(tokens[0].type === "AdditiveExpression" && tokens[1] && tokens[1].type === "EOF" ) {
+      let node = {
+          type:"Expression",
+          children:[tokens.shift(), tokens.shift()]
+      }
+      tokens.unshift(node);
+      return node;
+  }
+  AdditiveExpression(tokens);
+  return Expression(tokens);
+}
+
+
+function AdditiveExpression(tokens) {
+  if(tokens[0].type === "MultiplicativeExpression") {
+    let node = {
+      type:"AdditiveExpression",
+      children:[tokens[0]]
+    }
+    tokens[0] = node;
+    return node;
+  } else if(tokens[0].type === "AdditiveExpression" && tokens[1].type === '+') {
+    let node = {
+      type:"AdditiveExpression",
+      operator:"+",
+      children:[tokens.shift(), tokens.shift(), MultiplicativeExpression(tokens)]
+    }
+    tokens.unshift(node);
+  } else if(tokens[0].type === "AdditiveExpression" && tokens[1].type === '-') {
+    let node = {
+      type:"AdditiveExpression",
+      operator:"-",
+      children:[tokens.shift(), tokens.shift(), MultiplicativeExpression(tokens)]
+    }
+    tokens.unshift(node);
+  }
+}
+
+function MultiplicativeExpression(tokens) {
+
+}
+
+function evaluate(node) { // 计算过程
+  let {type, value, operator, children} = node
+  if (type === 'Expression') { // 表达式
+    return evaluate(children[0])
+  } else if (type === 'AdditiveExpression') { // +-
+    if (operator === '+') {
+      return evaluate(children[0]) + evaluate(children[2])
+    } else if (operator === '-') {
+      return evaluate(children[0]) - evaluate(children[2])
+    }
+    return evaluate(children[0])
+  } else if (type === 'MultiplicativeExpression') { // */
+    if (operator === '*') {
+      return evaluate(children[0]) * evaluate(children[2])
+    } else if (operator === '/') {
+      return evaluate(children[0]) / evaluate(children[2])
+    }
+    return evaluate(children[0])
+  } else if (type === 'Number') { // 纯数字
+    return Number(value)
+  }
+}

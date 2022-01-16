fd679527feb6cf65e24ece0daff20522b4714d2e
尤水就下 <lgq627628@163.com>
2022-01-16 17:14:40 +0800

    feat: 重构绘制函数和拖拽

--- 手写原生/function/main.ts
+++ 手写原生/function/main.ts
@@ -1,3 +1,5 @@
+import { IConfig, IState } from "./IConfig";
+
 export class FnApp {
     public canvas: HTMLCanvasElement;
     public ctx2d: CanvasRenderingContext2D | null;
@@ -5,9 +7,10 @@ export class FnApp {
     public height: number;
     public halfWidth: number;
     public halfHeight: number;
-    public gridLen: number = 50;
+    /** 每个网格的大小 */
+    public gridSize: Size = new Size(20, 20);
     public arrowLen: number = 10;
-    public steps: number;
+    public steps: number = 1;
     public isMouseDown: boolean = false;
     public isSupportMouseMove: boolean = false;
 
@@ -16,7 +19,33 @@ export class FnApp {
 
     public fnList: Function[] = [];
     public pointList: vec2[][] = [];
+
     
+    public sampleCount: number = 300;
+   
+   
+    public ratio: number = 2;
+    public gridCount: number = 10;
+
+    public config: IConfig = {
+        startX: -200,
+        endX: 200,
+        xLength: 0,
+        yLength: 0,
+        startY: 0,
+        endY: 0,
+        steps: 1,
+        scaleSteps: 0.1,
+        fontSize: 8,
+    };
+    public state: IState = {
+        startPos: null,
+        endPos: null,
+        translateX: 0,
+        translateY: 0,
+        scale: 1,
+    };
+
     constructor(canvas: HTMLCanvasElement, opts: any) {
         this.canvas = canvas;
         this.ctx2d = canvas.getContext('2d');
@@ -24,104 +53,92 @@ export class FnApp {
         this.height = canvas.height;
         this.halfWidth = canvas.width / 2;
         this.halfHeight = canvas.height / 2;
-        this.steps = 1;
+        this.config.xLength = this.config.endX - this.config.startX;
+        this.config.startY = -(this.config.xLength) * this.height / this.width / 2;
+        this.config.endY = -this.config.startY;
+        this.config.yLength = this.config.endY - this.config.startY;
 
+
+        this.state.translateX = this.halfWidth;
+        this.state.translateY = this.halfHeight;
+        this.state.scale = this.width / this.config.xLength;
+    
         this.canvas.addEventListener('mousedown', this, false);
         document.addEventListener('mouseup', this, false);
         this.canvas.addEventListener('mousemove', this, false);
         this.canvas.addEventListener('mousewheel', this, false);
 
-        this.drawGrid();
-        this.drawAxis();
-        this.drawUnitXY();
+        this.redraw();
     }
     addFn(fn: Function) {
         this.fnList.push(fn);
-        // this.ctx2d?.translate(this.halfHeight, this.halfHeight);
-        const points = this._calcPoints(fn);
-        points && this.pointList.push(points);
     }
     drawAxis() {
-        const { width, height, halfWidth, halfHeight, arrowLen } = this;
+        this.ctx2d?.save();
+        const { config: { startX, endX, startY, endY }, arrowLen } = this;
         // 绘制 x 轴
-        this.drawLine(0, halfHeight, width, halfHeight);
+        this.drawLine(startX, 0, endX, 0);
+        this.drawLine(20, 20, 100, 0);
         // 绘制 x 轴箭头
-        this.drawLine(width, halfHeight, width - arrowLen, halfHeight - arrowLen);
-        this.drawLine(width, halfHeight, width - arrowLen, halfHeight + arrowLen);
+        // this.drawLine(endX, 0, endX - arrowLen, -arrowLen);
+        // this.drawLine(endX, 0, endX - arrowLen, arrowLen);
         // 绘制 y 轴
-        this.drawLine(halfWidth, 0, halfWidth, height);
+        this.drawLine(0, startY, 0, endY);
         // 绘制 y 轴箭头
-        this.drawLine(halfWidth, 0, halfWidth - arrowLen, arrowLen);
-        this.drawLine(halfWidth, 0, halfWidth + arrowLen, arrowLen);
+        // this.drawLine(0, startY, -arrowLen, -endY + arrowLen);
+        // this.drawLine(0, startY, arrowLen, -endY + arrowLen);
+        this.ctx2d?.restore();
+    }
+    drawAxisLabel() {
+        const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
+        if (!ctx2d) return;
+        const { gridSize, config: { fontSize, startX, endX, startY, endY } } = this;
+
+        ctx2d.save();
+        ctx2d.font = `${fontSize}px sans-serif`;
+
+        const unitX = gridSize.w;
+        const unitY = gridSize.h;
+        // 绘制 x 正半轴刻度
+        for(let i = 0; i * unitX < endX; i++) {
+            const label: string = String(i * unitX);
+            ctx2d.fillText(label, i * gridSize.w - ctx2d.measureText(label).width / 2, fontSize);
+        }
+        // 绘制 x 负半轴刻度
+        for(let i = 0; i * unitX > startX; i--) {
+            const label: string = String(i * unitX);
+            ctx2d.fillText(label, i * gridSize.w - ctx2d.measureText(label).width / 2, fontSize);
+        }
+        // 绘制 y 正半轴刻度
+        for(let i = 0; i * unitY < endY; i++) {
+            const label: string = String(-i * unitY);
+            ctx2d.fillText(label, -ctx2d.measureText(label).width, i * gridSize.h + fontSize / 2);
+        }
+        // 绘制 y 负半轴刻度
+        for(let i = 0; i * unitY > startY; i--) {
+            const label: string = String(-i * unitY);
+            ctx2d.fillText(label, -ctx2d.measureText(label).width, i * gridSize.h + fontSize / 2);
+        }
+        ctx2d.restore();
     }
     drawGrid() {
         this.ctx2d?.save();
-        this.ctx2d?.setTransform(1, 0, 0, 1, 0, 0);
-        const { width, height, gridLen } = this;
-        // 绘制横线
-        for(let i = 0; i <= height; i += gridLen) {
-            this.drawLine(0, i, width, i, '#eee');
+        const { halfWidth, halfHeight, gridSize, config: { startX, startY, endX, endY } } = this;
+        // 绘制横线，从中心点开始向上向下画
+        for(let i = 0; i < endY; i += gridSize.h) {
+            this.drawLine(startX, i, endX, i, '#eee');
         }
-        // 绘制竖线
-        for(let i = 0; i < width; i += gridLen) {
-            this.drawLine(i, 0, i, height, '#eee');
+        for(let i = 0; i > startY; i -= gridSize.h) {
+            this.drawLine(startX, i, endX, i, '#eee');
+        }
+        // 绘制竖线，从中心点开始向左向右画
+        for(let i = 0; i < endX; i += gridSize.w) {
+            this.drawLine(i, startY, i, endY, '#eee');
+        }
+        for(let i = 0; i > startX; i -= gridSize.w) {
+            this.drawLine(i, startY, i, endY, '#eee');
         }
         this.ctx2d?.restore();
-    }
-    drawUnitXY() {
-        const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
-        if (!ctx2d) return;
-        const fontSize: number = 14;
-        const xCount = this.width / this.gridLen;
-        const yCount = this.height / this.gridLen;
-        ctx2d.font = `${fontSize}px sans-serif`;
-        for (let i = 1; i < xCount; i++) {
-            const label: string = String(i * this.gridLen - this.halfWidth);
-            ctx2d.fillText(label, i * this.gridLen - ctx2d.measureText(label).width / 2, this.halfHeight + fontSize);
-        }
-        for (let i = 1; i < yCount; i++) {
-            const label: string = String(i * this.gridLen - this.halfHeight);
-            ctx2d.fillText(label, this.halfWidth - ctx2d.measureText(label).width, i * this.gridLen + fontSize / 2);
-        }
-    }
-    draw() {
-        const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
-        if (!ctx2d) return;
-        ctx2d.save();
-        ctx2d.translate(this.width / 2, this.height / 2);
-        ctx2d.scale(1, -1);
-        this.fnList.forEach((_, i) => {
-            this.drawFn(this.pointList[i]);
-        });
-        ctx2d.restore();
-    }
-    drawFn(points: vec2[]) {
-        const color = '#' + Math.random().toString(16).slice(2, 8);
-        for (let i = 1; i < points.length; i++) {
-            const prevePoint = points[i - 1];
-            const curPoint = points[i];
-            this.drawLine(prevePoint.x, prevePoint.y, curPoint.x, curPoint.y, color);
-        }
-    }
-    _calcPoints(fn: Function) {
-        const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
-        if (!ctx2d) return;
-        const matrix: DOMMatrix = ctx2d.getTransform();
-        // const x: number = matrix.e;
-        // const y: number = matrix.f;
-        // const scaleX: number = 1 || matrix.a;
-        // const scaleY: number = 1000 || matrix.d;
-        // console.log(matrix, x, y);
-        const scaleX: number = 1;
-        const scaleY: number = 1;
-        const { halfWidth, steps } = this;
-        const points: vec2[] = [];
-        for(let x = -halfWidth; x <= halfWidth; x += steps) {
-            const y = fn(x) * scaleY;
-            points.push(new vec2(x, y));
-        }
-        console.log(points);
-        return points;
     }
     drawLine(x1: number, y1: number, x2: number, y2: number, color: string = '#000') {
         const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
@@ -135,43 +152,80 @@ export class FnApp {
     drawFnAnimation() {
     }
     redraw() {
-        const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
-        if (!ctx2d) return;
-        ctx2d.clearRect(0, 0, this.width, this.height);
+        this.ctx2d?.setTransform(1, 0, 0, 1, 0, 0);
+        this.ctx2d?.clearRect(0, 0, this.width, this.height);
+        // 变换坐标系
+        const { scale, translateX, translateY } = this.state;
+        this.ctx2d?.setTransform(scale, 0, 0, scale, translateX, translateY);
         this.drawGrid();
         this.drawAxis();
-        this.drawUnitXY();
+        this.drawAxisLabel();
         this.draw();
+    }
+    draw() {
+        const { scale, translateX, translateY } = this.state;
+        this.ctx2d?.setTransform(scale, 0, 0, -scale, translateX, translateY);
+        this.fnList.forEach(fn => {
+            this.drawFn(fn);
+        });
+    }
+    drawFn(fn: Function) {
+        const ctx2d: CanvasRenderingContext2D | null = this.ctx2d;
+        if (!ctx2d) return;
+
+        const { width, height, gridSize } = this;
+        const { startX, endX, steps } = this.config;
+
+        ctx2d.save();
+        ctx2d.beginPath();
+        ctx2d.strokeStyle = '#' + Math.random().toString(16).slice(2, 8);
+        for(let x = startX; x <= endX; x += steps) {
+            const y = fn(x);
+            if (x === startX) {
+                ctx2d.moveTo(x, y);
+            } else {
+                ctx2d.lineTo(x, y);
+            }
+        }
+        ctx2d.stroke();
+        ctx2d.restore();
     }
     dispatchMouseDown(e: MouseEvent, mousePos: vec2) {
         this.startPos = mousePos;
-        console.log('鼠标按下');
     }
     // dispatchMouseMove(e: Event, mousePos: vec2) {
-    //     console.log('鼠标移动');
     // }
     dispatchMouseDrag(e: MouseEvent, mousePos: vec2) {
         this.endPos = mousePos;
-        const diff = vec2.diff(this.startPos, this.endPos);
-        console.log('鼠标拖拽', diff);
-        this.ctx2d?.translate(diff.x, diff.y);
-        // this.ctx2d?.setTransform(1, 0, 0, 1, diff.x, diff.y);
+        const diff = vec2.diff(this.endPos, this.startPos);
+         // 往右边移动坐标系是往左的
+        this.state.translateX -= diff.x;
+        this.state.translateY -= diff.y;
+
+        diff.multi(1/this.state.scale);
+        this.config.startX += diff.x;
+        this.config.endX += diff.x;
+        this.config.startY += diff.y;
+        this.config.endY += diff.y;
+
+        // 重新渲染
         this.redraw();
         this.startPos = this.endPos;
     }
     dispatchMouseUp(e: MouseEvent, mousePos: vec2) {
-        console.log('鼠标抬起');
+        this.state.startPos = null;
+        this.state.endPos = null;
     }
     dispatchMouseWheel(e: WheelEvent, mousePos: vec2) {
-        console.log('鼠标滑动', e.deltaY);
+        e.preventDefault();
+        
+        let { config: { scaleSteps }, state: { scale } } = this;
         const { deltaY } = e;
-        let ratio: number = 1;
         if (deltaY > 0) {
-            ratio = -1 / 1.1;
+            scale -= scaleSteps;
         } else if (deltaY < 0) {
-            ratio = 1.1;
+            scale += scaleSteps;
         }
-        this.ctx2d?.scale(ratio, ratio);
         this.redraw();
     }
     public handleEvent(e: Event): void {
@@ -207,14 +261,27 @@ export class FnApp {
     }
 }
 
-class vec2 {
+export class vec2 {
     public x: number;
     public y: number;
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
     }
+    multi(scale: number) {
+        this.x = this.x * scale;
+        this.y = this.y * scale;
+    }
     static diff(v1: vec2, v2: vec2) {
         return new vec2(v2.x - v1.x, v2.y - v1.y);
+    }
+}
+
+export class Size {
+    public w: number;
+    public h: number;
+    constructor(w: number, h: number) {
+        this.w = w;
+        this.h = h;
     }
 }
